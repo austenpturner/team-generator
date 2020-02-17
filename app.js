dcd7bbc80d783ee971fd373d5dcba9649b42e56c
Austen Turner <austenpturner@msn.com>
2020-02-16 17:20:56 -0800

    add functionality to print team member info to HTML page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let memberId = 0;
 let managers = [];
 let engineers = [];
 let interns = [];
+let cards = [];
 
 inquireMember();
 
@@ -40,23 +41,20 @@ function inquireMember() {
         } else {
             inquireIntern(name, memberId, email);
         }
-    })
-}
+    });
+};
 
 function newManager(name, id, email, officeNumber) {
     managers.push(new Manager(name, id, email, officeNumber));
-    // console.log(managers);
-}
+};
 
 function newEngineer(name, id, email, github) {
     engineers.push(new Engineer(name, id, email, github));
-    // console.log(engineers);
-}
+};
 
 function newIntern(name, id, email, school) {
     interns.push(new Intern(name, id, email, school));
-    // console.log(interns);
-}
+};
 
 function inquireManager(name, memberId, email) {
     inquirer.prompt([
@@ -70,7 +68,7 @@ function inquireManager(name, memberId, email) {
     }).then(() => {
         inquireAgain();
     });
-}
+};
 
 function inquireEngineer(name, memberId, email) {
     inquirer.prompt([
@@ -83,7 +81,7 @@ function inquireEngineer(name, memberId, email) {
     }).then(() => {
         inquireAgain();
     });
-}
+};
 
 function inquireIntern(name, memberId, email) {
     inquirer.prompt([
@@ -96,7 +94,7 @@ function inquireIntern(name, memberId, email) {
     }).then(() => {
         inquireAgain();
     });
-}
+};
 
 function inquireAgain() {
     inquirer.prompt([
@@ -109,7 +107,63 @@ function inquireAgain() {
         if (moreMembers) {
             inquireMember();
         } else {
+            writeHTML(managers);
+            writeHTML(engineers);
+            writeHTML(interns);
+            renderTeamHTML();
             console.log('Thank you for entering all your team members. A team profile has been generated.');
         }
     });
 };
+
+function writeHTML(memberArray) {
+    memberArray.forEach( member => {
+        const keys = Object.keys(member);
+        const role = member.getRole();
+        const values = Object.values(member);
+        const cardHTML = `
+            <div class="card">
+                <h5 class="card-header">${values[0]}</h5>
+                <div class="card-body">
+                <h5 class="card-title">${role}</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${values[1]}</li>
+                        <li class="list-group-item">Email: <a href=mailto:${values[2]}>${values[2]}</a></li>
+                        <li class="list-group-item">${keys[3]}: ${values[3]}</li>
+                    </ul>
+                </div>
+            </div>`
+        cards.push(cardHTML);
+
+    })
+};
+
+function renderTeamHTML() {
+    let teamHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+    
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+    `;
+    cards.forEach( card => {
+        teamHTML += card;
+    })
+    teamHTML += `
+    </body>
+    </html>
+    `;
+
+    fs.writeFile('trial.html', teamHTML, (err) => {
+        if (err) {
+            return err;
+        }
+
+        console.log('written to trial.html');
+    })
+}
